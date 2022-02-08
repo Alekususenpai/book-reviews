c6ca20929e02a2e6a971d7fb2c74f00a7c3781b3
Alekususenpai <83718750+Alekususenpai@users.noreply.github.com>
2022-02-09 00:06:20 +0100

    db config

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-const dotenv = require('dotenv');
-dotenv.config();
-
+require('dotenv').config()
 const express = require('express');
 const app = express();
-
 
 
 //Database destructure / Connect //
